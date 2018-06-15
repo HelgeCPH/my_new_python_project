236b5112c5a4e1b1eaf7bbb2cf5b9f01f29199ed
Helge <rhp@cphbusiness.dk>
2018-06-15 10:45:16 +0200

    Added check for program execution from command-line call only.

--- cli_replace.py
+++ cli_replace.py
@@ -1,8 +1,9 @@
 import sys
 
 
-pattern = sys.argv[1]
-substitution = sys.argv[2]
+if __name__ == '__main__':
+    pattern = sys.argv[1]
+    substitution = sys.argv[2]
 
-for line in sys.stdin:
-    sys.stdout.write(line.replace(pattern, substitution))
+    for line in sys.stdin:
+        sys.stdout.write(line.replace(pattern, substitution))
